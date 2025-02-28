27f1349d991770c8f3ee0886594cb8d8a1b1ec1c
devarsung <devarsung@gmail.com>
2025-02-28 12:53:00 +0900

    jsp, 컨트롤러 매핑

--- semiproject/src/main/java/com/kh/semiproject/controller/NoticeController.java
+++ semiproject/src/main/java/com/kh/semiproject/controller/NoticeController.java
@@ -6,5 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/notice")
 public class NoticeController {
-
+	
+	@RequestMapping("/list")
+	public String list() {
+		return "/WEB-INF/views/notice/list.jsp";
+	}
+	
+	@RequestMapping("/detail")
+	public String detail() {
+		return "/WEB-INF/views/notice/detail.jsp";
+	}
 }
--- semiproject/src/main/java/com/kh/semiproject/controller/PlaceController.java
+++ semiproject/src/main/java/com/kh/semiproject/controller/PlaceController.java
@@ -6,5 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/place")
 public class PlaceController {
-
+	
+	@RequestMapping("/list")
+	public String list() {
+		return "/WEB-INF/views/place/list.jsp";
+	}
+	
+	@RequestMapping("/detail")
+	public String detail() {
+		return "/WEB-INF/views/place/detail.jsp";
+	}
 }
