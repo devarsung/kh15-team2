7b824a029afe6781a12665e93bb9bcbdc1253dc0
pyojaesohnkh15 <pjs.kh15@gmail.com>
2025-02-28 10:49:45 +0900

    Notice, Place, Review Dto 생성

---
+++ semiproject/src/main/java/com/kh/semiproject/dto/PlaceDto.java
@@ -0,0 +1,24 @@
+package com.kh.semiproject.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class PlaceDto {
+	private int placeNo;
+	private String placeTitle;
+	private String placeOverview;
+	private String placePost;
+	private String placeAddress1;
+	private String placeAddress2;
+	private int placeLike;
+	private int placeReview;
+	private Timestamp placeWtime;
+	private Timestamp placeEtime;
+	private String placeLegion;
+	private String placeWriter;
+}
