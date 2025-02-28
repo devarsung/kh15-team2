c876340de17ace03d648ec597a82cd2777b26896
jinyoungTest15 <jinyoung010402@gmail.com>
2025-02-28 11:09:40 +0900

    MemberMapper 생성

---
+++ semiproject/src/main/java/com/kh/semiproject/mapper/MemberMapper.java
@@ -0,0 +1,34 @@
+package com.kh.semiproject.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.semiproject.dto.MemberDto;
+
+@Component
+public class MemberMapper implements RowMapper<MemberDto>{
+
+	@Override
+	public MemberDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return MemberDto.builder()
+				.memberId(rs.getString("member_id"))
+				.memberPw(rs.getString("member_pw"))
+				.memberNickname(rs.getString("member_nickname"))
+				.memberBirth(rs.getString("member_birrh"))
+				.memberGender(rs.getString("member_gender"))
+				.memberContact(rs.getString("member_contact"))
+				.memberEmail(rs.getString("member_email"))
+				.memberPost(rs.getString("member_post"))
+				.memberAddress1(rs.getString("member_address1"))
+				.memberAddress2(rs.getString("member_address2"))
+				.memberLevel(rs.getString("member_level"))
+				.memberJoin(rs.getTimestamp("member_join"))
+				.memberLogin(rs.getTimestamp("member_login"))
+				.memberChange(rs.getTimestamp("member_change"))
+				.build();
+	}
+
+}
