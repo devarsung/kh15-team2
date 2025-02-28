245b2307c4597711f27b78d86d91b51cb89edc36
jinyoungTest15 <jinyoung010402@gmail.com>
2025-02-28 11:03:58 +0900

    memberDto 생성

--- semiproject/src/main/java/com/kh/semiproject/dto/MemberDto.java
+++ semiproject/src/main/java/com/kh/semiproject/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package com.kh.semiproject.dto;
 
+import java.sql.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +14,13 @@ public class MemberDto {
 	private String memberNickname;//닉네임
 	private String memberBirth;//생년월일
 	private String memberGender;//성별
+	private String memberContact;//전화번호
+	private String memberEmail;//이메일
+	private String memberPost;//우편번호
+	private String memberAddress1;//기본주소
+	private String memberAddress2;//상세주소
+	private String memberLevel;//회원등급
+	private Timestamp memberJoin;//회원가입일
+	private Timestamp memberLogin;//최종로그인일
+	private Timestamp memberChange;//비밀번호변경일
 }
