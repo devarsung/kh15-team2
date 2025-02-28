2621f20d8cfd41692408621dcc563daa0de31720
pyojaesohnkh15 <pjs.kh15@gmail.com>
2025-02-28 11:12:07 +0900

    Notice, Place, Review Mapper 생성

---
+++ semiproject/src/main/java/com/kh/semiproject/mapper/NoticeMapper.java
@@ -0,0 +1,26 @@
+package com.kh.semiproject.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.semiproject.dto.NoticeDto;
+
+@Component
+public class NoticeMapper implements RowMapper<NoticeDto>{
+
+	@Override
+	public NoticeDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return NoticeDto.builder()
+				.noticeNo(rs.getInt("notice_no"))
+				.noticeTitle(rs.getString("notice_title"))
+				.noticeContent(rs.getString("notice_content"))
+				.noticeWtime(rs.getTimestamp("notice_wtime"))
+				.noticeEtime(rs.getTimestamp("notice_etime"))
+				.noticeWriter(rs.getString("notice_writer"))
+				.build();
+	}
+
+}
