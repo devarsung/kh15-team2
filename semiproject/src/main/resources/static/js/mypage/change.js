4554184e8a75384d0645814dc7f65a4ee7c6a85d
ssw4111 <sswoo4111@gmail.com>
2025-03-10 15:57:32 +0900

    마이페이지수정창 주소변경 readonly해제시에만 클릭시 daum api불러옴

--- semiproject/src/main/resources/static/js/mypage/change.js
+++ semiproject/src/main/resources/static/js/mypage/change.js
@@ -94,6 +94,7 @@ $(function() {
 		$(this).val(convert);
 	});
 	$("[name=memberPost], [name=memberAddress1], .btn-address-search").click(function() {
+		if (!$("[name=memberPost]").prop('readonly') && !$("[name=memberAddress1]").prop('readonly')) {
 		new daum.Postcode({
 			oncomplete: function(data) {
 				// daum api
@@ -110,6 +111,7 @@ $(function() {
 				document.querySelector("[name=memberAddress2]").focus();
 			}
 		}).open();
+		}
 	});
 	$("[name=memberAddress2]").blur(function() {
 		var memberPost = $("[name=memberPost]").val();
