8d1af3f79bf4d33c8ac6b358b4420dd4428f8da7
jinyoungTest15 <jinyoung010402@gmail.com>
2025-03-10 16:04:18 +0900

    댓글 닉네임으로 구현 수정본

--- semiproject/src/main/java/com/kh/semiproject/dao/ReplyListViewDao.java
+++ semiproject/src/main/java/com/kh/semiproject/dao/ReplyListViewDao.java
@@ -16,19 +16,23 @@ public class ReplyListViewDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	public List<ReplyListViewDto> selectListByReviewNo(int reviewNo) {
-	    String sql = "SELECT r.reply_no, r.reply_writer, r.reply_content, r.reply_wtime, r.reply_etime, m.member_nickname " 
-	               + "FROM reply r "
-	               + "JOIN member m ON r.reply_writer = m.member_id "
-	               + "WHERE r.reply_origin = ?";
-	    Object[] data = { reviewNo};
-	    return jdbcTemplate.query(sql, replyListViewMapper, data);
-	}
-	
-	public String selectNicknameById(String userId) {
-	    String sql = "SELECT m.member_nickname FROM member m WHERE m.member_id = ?";
-	    return jdbcTemplate.queryForObject(sql, String.class, userId);
-	}
+	// 댓글 목록 조회 (닉네임 포함)
+    public List<ReplyListViewDto> selectList(int replyOrigin) {
+        String sql = "SELECT r.reply_no, m.member_nickname AS reply_writer, " 
+        		+"r.reply_origin, r.reply_content, r.reply_wtime, r.reply_etime " 
+        		+"FROM reply r "
+        		+"JOIN member m ON r.reply_writer = m.member_id " 
+	        	+"WHERE r.reply_origin = ? " 
+	        	+"ORDER BY r.reply_wtime DESC";
+        Object[] data = {replyOrigin};
+        return jdbcTemplate.query(sql, replyListViewMapper, data);
+    }
+    
+    public String selectNicknameById(String userId) {
+        String sql = "SELECT member_nickname FROM member WHERE member_id = ?";
+        return jdbcTemplate.queryForObject(sql, String.class, userId);
+    }
+
 }
 
 
