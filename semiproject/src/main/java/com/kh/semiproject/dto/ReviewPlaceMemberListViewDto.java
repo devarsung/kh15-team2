9234caa984c701b6fa621326df0ff41deff57261
pyojaesohnkh15 <pjs.kh15@gmail.com>
2025-03-11 12:25:06 +0900

    시간 표기

--- semiproject/src/main/java/com/kh/semiproject/dto/ReviewPlaceMemberListViewDto.java
+++ semiproject/src/main/java/com/kh/semiproject/dto/ReviewPlaceMemberListViewDto.java
@@ -1,6 +1,9 @@
 package com.kh.semiproject.dto;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,5 +29,27 @@ public class ReviewPlaceMemberListViewDto {
 	private String placeTitle;
 	
 	//private int totalLikesAndReads;
+	public String getWtimeString() {
+		LocalDate today = LocalDate.now();
+		LocalDateTime wtime = reviewWtime.toLocalDateTime();
+		LocalDate wdate = wtime.toLocalDate();
+		if(wdate.isBefore(today)) {
+			return wdate.toString();
+		}
+		else {
+			return wtime.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+	}	
 	
+	public String getEtimeString() {
+		LocalDate today = LocalDate.now();
+		LocalDateTime wtime = reviewWtime.toLocalDateTime();
+		LocalDate wdate = wtime.toLocalDate();
+		if(wdate.isBefore(today)) {
+			return wdate.toString();
+		}
+		else {
+			return wtime.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+	}	
 }
