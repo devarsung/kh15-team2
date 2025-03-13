a8203d7b7a56007edb8a760b9131045880d5f7ff
jinyoungTest15 <jinyoung010402@gmail.com>
2025-03-13 14:52:29 +0900

    작성일 시간 수정

--- semiproject/src/main/java/com/kh/semiproject/dto/ReviewLikeDto.java
+++ semiproject/src/main/java/com/kh/semiproject/dto/ReviewLikeDto.java
@@ -1,6 +1,9 @@
 package com.kh.semiproject.dto;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,4 +20,28 @@ public class ReviewLikeDto {
 	private Timestamp reviewWtime;
 	private int reviewRead;
 	//private Timestamp reviewLikeTime;
+	
+	public String getWtimeString() {
+		LocalDate today = LocalDate.now();
+		LocalDateTime wtime = reviewWtime.toLocalDateTime();
+		LocalDate wdate = wtime.toLocalDate();
+		if(wdate.isBefore(today)) {
+			return wdate.toString();
+		}
+		else {
+			return wtime.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+	}	
+	
+	public String getEtimeString() {
+		LocalDate today = LocalDate.now();
+		LocalDateTime wtime = reviewWtime.toLocalDateTime();
+		LocalDate wdate = wtime.toLocalDate();
+		if(wdate.isBefore(today)) {
+			return wdate.toString();
+		}
+		else {
+			return wtime.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+	}	
 }
